18c352521ce10a08196d1dda48e510d16e9a27d9
Molele Kishoez <molelekishoez@gmail.com>
2026-06-24 11:19:55 +0200

    feat: add Inngest API route handler with user synchronization functions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
+import { syncUserDeletion, syncUserUpdation, syncUserCreation } from "@inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [],
+  functions: [
+    syncUserDeletion,
+      syncUserUpdation,
+    syncUserCreation,
+  ],
 });
