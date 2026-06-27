95b1197e37121c2dbbf8395b5a95d3577604f429
Molele Kishoez <molelekishoez@gmail.com>
2026-06-28 01:35:30 +0200

    feat: initialize Inngest client and configure API route for user synchronization functions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { syncUserDeletion, syncUserUpdation, syncUserCreation } from "@/inngest/functions";
+import {
+  syncUserDeletion,
+  syncUserUpdation,
+  syncUserCreation,
+} from "@/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    syncUserDeletion,
-      syncUserUpdation,
-    syncUserCreation,
-  ],
+  functions: [syncUserDeletion, syncUserUpdation, syncUserCreation],
 });
